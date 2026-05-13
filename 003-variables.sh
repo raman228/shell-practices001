0c6f443cb2cc16d930825d33d0582425f0eba9fd
raman <ramanj786@gmail.com>
2026-05-13 17:45:48 +0530

    variables

---
+++ 003-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#No space before or after equal.
+
+PERSON1="Sachin"
+PERSON2="Rahul"
+
+echo "$PESON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine, how are you ?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
+
