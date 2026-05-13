361f1d237b3a06a7e8044b55e171403e80834fff
raman <ramanj786@gmail.com>
2026-05-13 18:18:40 +0530

    variabless2

--- 004-variables.sh
+++ 004-variables.sh
@@ -5,8 +5,8 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1":: Welcome to Alaska $PERSON2
-echo "$PERSON2":: Thank you $PERSON1, Finally not on zoom webinar"
-echo "$PERSON1":: SO, do we have a deal"
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom webinar"
+echo "$PERSON1:: SO, do we have a deal"
 echo "$PERSON2:: Yes, if you make ukraine joins Russia"
 
