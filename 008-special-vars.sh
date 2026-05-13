8d35e85fdf1a1e086c80d477069cf57580609f9f
raman <ramanj786@gmail.com>
2026-05-13 19:59:57 +0530

    special-vars

---
+++ 008-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script name $0"
+echo "Current directory: $PWD"
+echo "who is runnig this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+slee 50 &
+echo "PID of the last command in background is: $!"
+
