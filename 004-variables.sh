0666222366e28d40fe0cde1fef3fca18f8d27de4
raman <ramanj786@gmail.com>
2026-05-13 18:12:59 +0530

    variables2

---
+++ 004-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Arguments
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1":: Welcome to Alaska $PERSON2
+echo "$PERSON2":: Thank you $PERSON1, Finally not on zoom webinar"
+echo "$PERSON1":: SO, do we have a deal"
+echo "$PERSON2:: Yes, if you make ukraine joins Russia"
+
