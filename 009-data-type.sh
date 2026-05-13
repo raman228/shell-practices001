c6038af6e099133d941b7fc9ffb01f4c09926583
raman <ramanj786@gmail.com>
2026-05-13 22:13:52 +0530

    datatype

---
+++ 009-data-type.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#everything in shell is considerd as string
+
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+echo "SUM is: ${SUM}"
+
+#size 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First leaders: ${LEADERS[0]}"
+echo "First leaders: ${LEADERS[10]}"
+
+
