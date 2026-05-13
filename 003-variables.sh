9d71aa810645096c56d422e2513cca8690ab3fad
raman <ramanj786@gmail.com>
2026-05-13 18:01:53 +0530

    variabless

--- 003-variables.sh
+++ 003-variables.sh
@@ -5,8 +5,8 @@
 PERSON1="Sachin"
 PERSON2="Rahul"
 
-echo "$PESON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, I am fine, how are you ?"
-echo "$PERSON1:: what are you learning recently?"
-echo "$PERSON2:: I am learning shell scripting $PERSON1"
+echo "$PESON1::Hi $PERSON2, How are you?"
+echo "$PERSON2::Hello $PERSON1, I am fine, how are you ?"
+echo "$PERSON1::what are you learning recently?"
+echo "$PERSON2::I am learning shell scripting $PERSON1"
 
