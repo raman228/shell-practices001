44c311fd051adf13ac2f69e07595b36faf99c8f4
raman <ramanj786@gmail.com>
2026-05-14 14:16:09 +0530

    evenandodd

---
+++ 0011-condition.sh
@@ -0,0 +1,13 @@
+echo "Please enter the number"
+read NUMBER
+
+
+if [ $(($NUMBER % 2)) -eq 0 ]; the
+     echo " Give number $NUMBER is: EVEN"
+else
+     echo " Give number $NUMBER is: ODD"
+fi
+
+
+
+    
