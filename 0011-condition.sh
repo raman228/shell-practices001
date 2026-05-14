4a700f0f4c9d5d96c65a1d7cc2fbd35a870decf4
raman <ramanj786@gmail.com>
2026-05-14 14:28:09 +0530

    newcommitodd

--- 0011-condition.sh
+++ 0011-condition.sh
@@ -2,7 +2,7 @@ echo "Please enter the number"
 read NUMBER
 
 
-if [ $(($NUMBER % 2)) -eq 0 ]; the
+if [ $(($NUMBER % 2)) -eq 0 ]; then
      echo " Give number $NUMBER is: EVEN"
 else
      echo " Give number $NUMBER is: ODD"
