0063a16254dbca7af2508a49df7a815813b4bbec
raman <ramanj786@gmail.com>
2026-05-14 14:12:58 +0530

    odd even number

--- 0010-condition.sh
+++ 0010-condition.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
-     echo "Give number $NUMBER is less than 10"
+    echo "Give number $NUMBER is less than 10"
 elif [ $NUMBER -eq 10 ]; then
     echo "Given number $NUMBER is equal to 10"
 else
