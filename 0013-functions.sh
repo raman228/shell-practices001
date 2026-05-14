ce24de1d4af05b250d0a58953032d27edcb46cbe
raman <ramanj786@gmail.com>
2026-05-14 17:19:21 +0530

    functions

--- 0013-functions.sh
+++ 0013-functions.sh
@@ -8,7 +8,7 @@ if [ $USERID -ne 0 ]; then
 
 fi
 
-VALIDATE() { $ functions receive inputs through args just like shell script args
+VALIDATE() { # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
         echo "ERROR:: Installing $2 is failure"
         exit 1
